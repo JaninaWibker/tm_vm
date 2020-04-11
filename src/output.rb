38caae45d6e684ed7dc9b8d271607a59f15f3a2c
Jannik Wibker <jannikwibker@gmail.com>
2020-04-11 19:25:35 +0200

    now properly generating svgs (not controlled by cli options at the moment, cleanup / improvement will come soon)

--- src/output.rb
+++ src/output.rb
@@ -3,10 +3,20 @@ require 'tmpdir'
 
 require_relative './tm_re.rb'
 
+def tex2svg(basedir, basename)
+  Dir.mktmpdir { |dir|
+    system "latex -interaction=nonstopmode -output-directory=#{dir} #{File.join(basedir, basename + '.tex')} >/dev/null"
+    system "dvisvgm --output=#{File.join(basedir, basename)}.svg #{File.join(dir, basename)}.dvi 2&>/dev/null"
+  }
+end
+
 def mkdir_if_not_exists(path)
   dirname = File.join(Dir.pwd, path)
   Dir.mkdir dirname unless File.exists?(dirname)
 end
+
+# TODO: differentiate between different output formats
+# TODO: implement different output formats
 
 def output_begin(tm, options)
   template = nil
@@ -23,8 +33,6 @@ def output_begin(tm, options)
 end
 
 def output_stream(output, tm, options, state)
-
-  puts state
 
   symbol_re_prepared = state[:symbol] == nil ? 'BLANK' : state[:symbol].include?("'") ?
     ('"' + state[:symbol] + '"') :
@@ -55,7 +63,9 @@ end
 
 def output_end(output, tm, options)
   output[:output].each_with_index { |content, index|
-    filepath = File.join(Dir.pwd, "output", File.basename(options.filepath) + "-#{index}.tex")
-    File.write(filepath, content)
+    basedir = File.join(Dir.pwd, "output")
+    basename = File.basename(options.filepath) + "-#{index}"
+    File.write(File.join(basedir, basename + ".tex"), content)
+    tex2svg(basedir, basename)
   }
 end
